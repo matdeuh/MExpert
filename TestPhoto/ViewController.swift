8e37478fd038b8f848b1e2ebc2c78171cd7bceed
Wilson <Wilson@macbook-pro-de-wilson.local>
2016-05-12 14:14:46 +0200

    Initial Commit

---
+++ TestPhoto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestPhoto
+//
+//  Created by Wilson on 12/05/2016.
+//  Copyright © 2016 Wilson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
